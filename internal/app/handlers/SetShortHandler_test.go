8a52543ed5a23c3a9615cd23d8bf29dc344a06e9
vitaly <lobov.vitaliy@gmail.com>
2024-01-04 13:59:08 +0300

    iter2

--- internal/app/handlers/SetShortHandler_test.go
+++ internal/app/handlers/SetShortHandler_test.go
@@ -1,73 +1,65 @@
 package handlers
 
-//
-//import (
-//	"github.com/go-chi/chi"
-//	"github.com/stretchr/testify/assert"
-//	"github.com/stretchr/testify/require"
-//	"io"
-//	"net/http"
-//	"net/http/httptest"
-//	"testing"
-//)
-//
-//func testRequest(t *testing.T, ts *httptest.Server, method,
-//	path string) (*http.Response, string) {
-//	req, err := http.NewRequest(method, ts.URL+path, nil)
-//	require.NoError(t, err)
-//
-//	resp, err := ts.Client().Do(req)
-//	require.NoError(t, err)
-//	defer resp.Body.Close()
-//
-//	respBody, err := io.ReadAll(resp.Body)
-//	require.NoError(t, err)
-//
-//	return resp, string(respBody)
-//}
-//
-//func TestRouter(t *testing.T) {
-//	//ts := httptest.NewServer(CarRouter())
-//	mux := chi.NewRouter()
-//	mux.Post("/update/{type}/{name}/{value}", UpdateHandler)
-//	ts := httptest.NewServer(mux)
-//	defer ts.Close()
-//	// ниже добавим тестовые запросы
-//	// ...
-//}
-//
-//func TestUpdateHandler(t *testing.T) {
-//	type want struct {
-//		contentType string
-//		statusCode  int
-//	}
-//	tests := []struct {
-//		name  string
-//		metod string
-//		path  string
-//		want  want
-//	}{
-//		{
-//			name:  "test1",
-//			metod: http.MethodPost,
-//			path:  "/update/counter/someMetric/527",
-//			want: want{
-//				contentType: "text/plain",
-//				statusCode:  200,
-//			},
-//		},
-//	}
-//	mux := chi.NewRouter()
-//	mux.Post("/update/{type}/{name}/{value}", UpdateHandler)
-//	ts := httptest.NewServer(mux)
-//
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//
-//			resp, _ := testRequest(t, ts, tt.metod, tt.path)
-//			assert.Equal(t, tt.want.statusCode, resp.StatusCode)
-//			assert.Equal(t, tt.want.contentType, resp.Header.Get("Content-Type"))
-//			resp.Body.Close()
-//		})
-//	}
-//}
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func testRequest(t *testing.T, ts *httptest.Server, method,
+	path string, body io.Reader) (*http.Response, string) {
+	req, err := http.NewRequest(method, ts.URL+path, body)
+	require.NoError(t, err)
+
+	resp, err := ts.Client().Do(req)
+	require.NoError(t, err)
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	return resp, string(respBody)
+}
+
+func TestUpdateHandler(t *testing.T) {
+	type want struct {
+		contentType string
+		statusCode  int
+	}
+	tests := []struct {
+		name  string
+		metod string
+		path  string
+		body  string
+		want  want
+	}{
+		{
+			name:  "test1",
+			metod: http.MethodPost,
+			path:  "/",
+			body:  "www.ya.ru",
+			want: want{
+				contentType: "text/plain; charset=utf-8",
+				statusCode:  201,
+			},
+		},
+	}
+
+	mux := chi.NewRouter()
+	mux.Post("/", SetShortHandler)
+	ts := httptest.NewServer(mux)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resp, _ := testRequest(t, ts, tt.metod, tt.path, strings.NewReader(tt.body))
+			assert.Equal(t, tt.want.statusCode, resp.StatusCode)
+			assert.Equal(t, tt.want.contentType, resp.Header.Get("Content-Type"))
+			resp.Body.Close()
+		})
+	}
+}
